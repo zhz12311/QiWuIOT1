d07a242994b79c8474fc867df29c995d528a051f
leiiiihuang <2379200654@qq.com>
2019-02-15 11:53:48 +0800

    adsa

---
+++ Eureak-Service/src/test/java/com/keenyoda/shiyan/iot/eureakservice/EureakServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.keenyoda.shiyan.iot.eureakservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EureakServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
